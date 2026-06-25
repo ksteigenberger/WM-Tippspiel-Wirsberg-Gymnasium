9255eaa4abf1d2ed9f0d8536cfd0b42a5f591a25
ksteigenberger <ksteigenberger@googlemail.com>
2026-06-25 13:22:09 +0200

    Create avatars.js with avatar file names
    Added a list of avatar file names for use in the application.

---
+++ avatare/avatars.js
@@ -0,0 +1,10 @@
+window.avatarFiles = [
+  "Bellingham.png",
+  "DennisUndav.png",
+  "LamineYamal.png",
+  "ManuelNeuer.png",
+  "Mbappe.png",
+  "Messi.png",
+  "Ronaldo.png"
+];
+//Bitte immer wenn neue Avatare hinzugefügt werden, den Dateinamen hier eintragen.
